c78e67f65da5fdfa845e1cc9106df61a86abd930
Scott Chamberlain <myrmecocystus@gmail.com>
2011-12-20 09:03:49 -0600

    added getOption to assign your gauges key instead of specifying it directly

--- gauges.R
+++ gauges.R
@@ -1,22 +1,35 @@
 ## Gaug.es API for blogs/websites
 require(rJava); require(RCurl); require(RJSONIO)
 
+key <- getOption("GaugesKey")
+basecall <- paste('curl -H', " '", 'X-Gauges-Token:', key, "' ", sep='')
+myinfo <- 'https://secure.gaug.es/me'
+clients <- 'https://secure.gaug.es/clients'
+gauges <- 'https://secure.gaug.es/gauges'
+content <- 'https://secure.gaug.es/gauges/:id/content'
+referrers <- 'https://secure.gaug.es/gauges/:id/referrers'
+
 # your information
-me<-'curl -H "X-Gauges-Token: 6e57f116f7933ee6c4d76efcc213081b" https://secure.gaug.es/me'
+me <- paste(basecall, myinfo, sep='')
 meout <- system(me, intern=T)
 fromJSON(meout)
 
 # API client list
-clientlist<-'curl -H "X-Gauges-Token: 6e57f116f7933ee6c4d76efcc213081b" https://secure.gaug.es/clients'
+clientlist <- paste(basecall, clients, sep='')
 clientlistout <- system(clientlist, intern=T)
 fromJSON(clientlistout)
 
 # Gauges list
-gaugeslist<-'curl -H "X-Gauges-Token: 6e57f116f7933ee6c4d76efcc213081b" https://secure.gaug.es/gauges'
+gaugeslist <- paste(basecall, gauges, sep='')
 gaugeslistout <- system(gaugeslist, intern=T)
 fromJSON(gaugeslistout)
 
 # Top content
-topcontent<-'curl -H "X-Gauges-Token: 6e57f116f7933ee6c4d76efcc213081b" https://secure.gaug.es/gauges/:id/content'
+topcontent <- paste(basecall, content, sep='')
 topcontenttout <- system(topcontent, intern=T)
 fromJSON(topcontenttout)
+
+# Referrers
+referrers_ <- paste(basecall, referrers, sep='')
+referrers_out <- system(referrers_, intern=T)
+fromJSON(referrers_out)
