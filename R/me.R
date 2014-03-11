a0cc44986b18e031a3158f1a1f31426be6f561f7
Scott Chamberlain <myrmecocystus@gmail.com>
2014-03-11 13:31:49 -0700

    update man files, small change to gs_me to pass in curl options

--- R/me.R
+++ R/me.R
@@ -4,13 +4,14 @@
 #' @param keyname Your API key name in your .Rprofile file
 #' @examples \dontrun{
 #' gs_me()
+#' gs_me(callopts=verbose())
 #' }
 #' @export
-gs_me <- function(keyname='GaugesKey')
+gs_me <- function(keyname='GaugesKey', callopts=list())
 {
   key <- getOption(keyname, stop("you need an API key for Gaug.es data"))
   url <- 'https://secure.gaug.es/me'
-  tt <- GET(url=url, config=list(httpheader=paste0('X-Gauges-Token:',key)))
+  tt <- GET(url=url, config=c(httpheader=paste0('X-Gauges-Token: ',key), callopts))
   stop_for_status(tt)
   content(tt)
 }
