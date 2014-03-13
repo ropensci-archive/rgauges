6e2a40ef3ba21702b790df2f48b292bdb2e997b9
Scott Chamberlain <myrmecocystus@gmail.com>
2014-03-13 08:59:54 -0700

    updated gs_me to proper header construction

--- R/me.R
+++ R/me.R
@@ -1,17 +1,18 @@
 #' Information on yourself.
 #' 
 #' @template all
+#' @export
 #' @param keyname Your API key name in your .Rprofile file
+#' @param callopts Curl debugging options passed in to httr::GET
 #' @examples \dontrun{
 #' gs_me()
 #' gs_me(callopts=verbose())
 #' }
-#' @export
 gs_me <- function(keyname='GaugesKey', callopts=list())
 {
   key <- getOption(keyname, stop("you need an API key for Gaug.es data"))
   url <- 'https://secure.gaug.es/me'
-  tt <- GET(url=url, config=c(httpheader=paste0('X-Gauges-Token: ',key), callopts))
+  tt <- GET(url=url, config=c(add_headers('X-Gauges-Token' = key), callopts))
   stop_for_status(tt)
   content(tt)
 }
